96ad3d27d1a07d40ab7e5034c37de49f9d35afd4
tubt4 <36790617+thnhtus@users.noreply.github.com>
2021-06-17 01:09:39 +0700

    add sidebar

---
+++ src/components/SideNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <div className="side-nav">
+      <h1>SideBar</h1>
+      <ul className="sidebar-list">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideNav;
